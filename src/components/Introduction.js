3d0fd69fe65781381c6d2d580ba7568b56a8ddb6
binh123440 <123440.lhbinh@gmail.com>
2025-03-20 16:36:50 +0700

    update_intro

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,15 +6,37 @@ const Introduction = () => {
     <div className="introduction">
       <div className="content">
         <div className="image">
-          <img src="path/to/your/image.jpg" alt="Founder" />
+          <img src="founder.jpg" alt="Founder" />
         </div>
         <div className="text">
-          <h3>Xin chào, Kuzuzangpo la, Tashi Delek, Hello...</h3>
-          <h2>Chào mừng đến với The Happy Firm</h2>
+          <h1>LOTUS VOYAGES - VÌ MỘT HÀNH TINH XANH</h1>
+          <h3>Thư ngỏ</h3>
+          <p>Bạn thân mến,</p>
           <p>
-            Là những chuyến đi, hành trình, trải nghiệm đến những vùng đất thú vị đầy năng lượng trên thế giới.
+            Với nguyện ước đóng góp phần mình cho đất nước đẹp tươi, thông qua việc tổ chức những chuyến du lịch khám phá đầy ý nghĩa, trong sự lựa chọn lối sống xanh và lành, chúng tôi đã bắt đầu thực hiện dự án thành lập CÔNG TY TNHH DỊCH VỤ & DU LỊCH QUỐC TẾ LOTUS VOYAGES vào ngày 26/08/2022 vừa qua.
           </p>
-          <button>Tìm hiểu thêm</button>
+          <p>
+            Chúng tôi thật sự hạnh phúc khi mà dự án ấp ủ bấy lâu nay của chúng tôi đã thành hiện thực.
+          </p>
+          <p>
+            Ước mơ tổ chức các chuyến du lịch chăm sóc sức khoẻ và phát triển những tài năng của bản thân, chúng tôi sẽ tổ chức các tour du lịch gồm workshop yoga, thiền tập, nấu ăn thuần chay, hội họa…
+          </p>
+          <p>
+            Chúng tôi hy vọng, trên hành trình đi về nẻo đường sáng, giúp cho cộng đồng người ăn thuần chay được rộng lớn hơn, giữ gìn đất mẹ và bảo vệ các loài động vật, chúng tôi sẽ nhận được sự ủng hộ của các bạn.
+          </p>
+          <p>
+            Rất mong các bạn cùng chung tay xây dựng một cộng đồng sống khoẻ, xanh và lành.
+          </p>
+          <p>
+            Chúng tôi xin gửi những lời tri ân chân thành đến gia đình và những người bạn đã ủng hộ về mặt tinh thần để doanh nghiệp chúng tôi được ra đời như ngày hôm nay.
+          </p>
+          <p>Với lòng biết ơn và sự trân quý!</p>
+          <p>Lotus Voyages,</p>
+          <p>12/09/2022.</p>
+          <div className="signature">
+            <p>Ms. Đặng Thị Liên</p>
+            <p>Founder</p>
+          </div>
         </div>
       </div>
     </div>
